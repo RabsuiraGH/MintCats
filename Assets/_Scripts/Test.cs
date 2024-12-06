b2615662beb783ae9570710f6a8ab2adaf89e686
RabsuiraGH <rabsuira@proton.me>
2024-12-07 00:17:27 +0300

    Added hot reload

--- Assets/_Scripts/Test.cs
+++ Assets/_Scripts/Test.cs
@@ -3,14 +3,22 @@ using Core.Character.Player;
 using Core.GameEventSystem;
 using UnityEngine;
 
-public class EventBusExample : MonoBehaviour
+public class Test : MonoBehaviour
 {
-    public PlayerInputManager PlayerManager;
-
-    public Transform cube;
+    [SerializeField] private Transform _cube;
+    [SerializeField] private Transform _cube2;
+    [SerializeField] private Transform _cube3;
 
     private void Update()
     {
-        cube.transform.position = cube.position + cube.forward * PlayerManager.ZoomInput;
+        Debug.Log(($"New dl"));
+        MoveCubes();
+    }
+
+    private void MoveCubes()
+    {
+        _cube.Translate(_cube.up * Time.deltaTime * 1f);
+        _cube2.Translate(_cube2.forward * Time.deltaTime * 1f);
+        _cube3.Translate(_cube3.right * Time.deltaTime * 1f);
     }
 }
